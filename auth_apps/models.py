634d30c3bbea4c4ef725799c8c7dd9b8345304ee
pemochamdev <pemochamdev@gmail.com>
2023-11-03 09:18:30 +0000

    User models

--- auth_apps/models.py
+++ auth_apps/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+
+
+class User(AbstractUser):
+    CREATOR = 'CREATOR'
+    SUBSCRIBER = 'SUBSCRIBER'
+
+    ROLE_CHOICES =(
+        (CREATOR, 'Créateur'),
+        (SUBSCRIBER, 'Abonné'),
+    )
+
+    role = models.CharField(max_length=15, choices=ROLE_CHOICES)
+    profile_photo = models.ImageField(upload_to='profile')
+    
